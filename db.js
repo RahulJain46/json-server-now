8e60949ea863535386de162f5f37bcdc72811ac7
rahuljain46 <rahuljain820436@gmail.com>
2020-05-05 16:02:57 +0530

    server

--- db.js
+++ db.js
@@ -1,12 +1,29 @@
-var jsonServer = require('json-server');
-var server = jsonServer.create();
-var router = jsonServer.router('db.json');
-var middlewares = jsonServer.defaults();
-var port = process.env.PORT || 5000;
-
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 7000;
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept,Origin,Content-Type,X-LS-CORS-Template,X-LS-Auth-Token,X-LS-Auth-User-Token,Content-Type,X-LS-Sync-Result,X-LS-Sequence,token"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST");
+  if (req.method === "POST") {
+    var clientIp = requestIp.getClientIp(req);
+    console.log(clientIp);
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
 server.use(router);
-server.listen(port, function () {
-  console.log('JSON Server is running');
-})
+
+server.listen(port);
